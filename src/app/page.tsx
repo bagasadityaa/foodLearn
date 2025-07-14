d6cc73bef1559fe5c7e65471e9b85c37e58a3d36
bagas aditya <bagasaditya599@gmail.com>
2025-07-15 00:47:23 +0700

    feat: integrate Supabase for authentication and theme management
    - Added Supabase dependencies to package.json and package-lock.json
    - Implemented ThemeProvider for theme management in layout.tsx
    - Created DarkmodeToggle component for theme switching
    - Set up environment configuration for Supabase in environment.ts
    - Developed Supabase client creation functions in client.ts and server.ts
    - Updated middleware for session management with Supabase
    - Added middleware configuration for Next.js routing

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,12 @@
+import { DarkmodeToggle } from "@/components/common/darkmode-toggle";
 import { Button } from "@/components/ui/button";
 import Image from "next/image";
 
 export default function Home() {
   return (
     <div >
-      <Button >Hello</Button>
+      <Button className="bg-red-400 dark:bg-amber-300">Hello</Button>
+      <DarkmodeToggle />
     </div>
   );
 }
